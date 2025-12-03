0f2bcc0208bc8061d848ff134cefd561dd6ba9dc
nassimrafrafi <nassimrafrafi3@gmail.com>
2025-12-03 09:32:39 +0100

    a

---
+++ src/main/java/com/example/nassim/NassimApplication.java
@@ -0,0 +1,13 @@
+package com.example.nassim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NassimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NassimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nassim/NassimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nassim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NassimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
